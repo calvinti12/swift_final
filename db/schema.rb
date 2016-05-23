1415cdbe1de9471a0479d8c78abea9bb5796be58
rhman <abdalrhmansudan@gmail.com>
2016-05-23 13:26:22 +0800

    fix clearance and install figaro

--- db/schema.rb
+++ db/schema.rb
@@ -11,36 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522152837) do
+ActiveRecord::Schema.define(version: 20160523050359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "preferences", force: :cascade do |t|
-    t.string   "avatar"
-    t.string   "nick_name"
-    t.integer  "age"
-    t.string   "country"
-    t.string   "state"
-    t.string   "description"
-    t.string   "gender"
-    t.string   "preference"
-    t.integer  "contact_num"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.date     "birthdate"
-    t.string   "email"
-    t.string   "password"
+    t.date     "birthday"
+    t.string   "description"
+    t.string   "country"
+    t.string   "gender"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.string   "encrypted_password", limit: 128
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128
+    t.string   "remember_token",     limit: 128, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
