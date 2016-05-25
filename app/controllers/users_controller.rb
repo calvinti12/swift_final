5f7e2c420f37329e558b8556951ca20ea780f2a2
Brand8888 <brandpong90@gmail.com>
2016-05-25 17:02:25 +0800

    Added Video Icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,7 @@ class UsersController < Clearance::UsersController
   end
 
   def update
-<<<<<<< HEAD
-=======
 
->>>>>>> master
 	@user = User.find(params[:id])
 	@user.update(permit_params)
 	 if @user.save
