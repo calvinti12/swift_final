03f14c3a6fa4fd25c82fcade0e1c60d302132e75
Brand8888 <brandpong90@gmail.com>
2016-05-28 15:26:26 +0800

    fixed landing page

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20160526095356) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "age"
+    t.date     "birthday"
     t.string   "description"
     t.string   "country"
     t.string   "state"
@@ -48,15 +48,13 @@ ActiveRecord::Schema.define(version: 20160526095356) do
     t.string   "prefer_gender"
     t.string   "prefer_country"
     t.string   "prefer_state"
-    t.integer  "prefer_start_age"
-    t.integer  "prefer_end_age"
+    t.integer  "prefer_age"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.string   "email",                          null: false
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
-    t.integer  "room_id"
     t.string   "image"
   end
 
