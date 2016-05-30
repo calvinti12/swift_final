b1580b662858d158b1bd0359d851e30453350f74
dvdhoek <djpvanderhoek@gmail.com>
2016-05-30 11:33:58 +0800

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -22,5 +22,4 @@ CarrierWave.configure do |config|
   }
 
   # Optional: Signing of download urls, e.g. for serving private content through CloudFront.
-  config.aws_signer = -> (unsigned_url, options) { Aws::CF::Signer.sign_url unsigned_url, options }
 end
